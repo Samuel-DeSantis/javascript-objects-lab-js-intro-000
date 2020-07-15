c5949aa4c66c4bb2206807cdf7e99b5a19e952e3
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-07-15 03:45:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-function updateObjectWithKeyAndValue(object, key, value) {return object[key] = value;}
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+function updateObjectWithKeyAndValue(object, key, value) {
   var newObj = Object.assign({}, object);
   newObj[key] = value;
   return newObj;
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {return object[key] = value;}
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
   return delete newObj[key] ? newObj : pass;
 }
-
 function destructivelyDeleteFromObjectByKey(object, key) {return delete object[key] ? object : pass;}
